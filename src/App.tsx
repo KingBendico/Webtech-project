359e0211514713c65bf932f40865f9011ae215a6
KingBendico <cph-bc105@cphbusiness.dk>
2023-02-24 09:28:01 +0100

    Hopefully netlify will accept this. Also made the table nicer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import React, { useState, useEffect } from 'react';
 
+interface Item {
+  name: string;
+  quantity: number;
+  price: number;
+}
+
 function App() {
   return (
     <div>
@@ -10,26 +16,50 @@ function App() {
   );
 }
 
+
+
 function ItemList() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
     fetch('/json_data/cart_items.json')
       .then(response => response.json())
-      .then(data => setItems(data));
+      .then((data: Item[]) => setItems(data));
   }, []);
+
+  const handleDeleteItem = (index: number) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  }
 
   return (
     <div>
-      <h1>List of Items</h1>
-      <ol>
-        {items.map((item, index) => (
-          <li key={index}>{item.name} {item.price}  {item.currency} </li>
-        ))}
-      </ol>
+      <h1>Shopping Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Item name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price} kr.</td>
+              <td><button onClick={() => handleDeleteItem(index)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
+
 
 
 export default App;
