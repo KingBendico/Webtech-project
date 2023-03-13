d3b2cf1d669f0d4f799ac19aec9fa61c060ab7c1
Christoffer Foldager <christoffer@decuria.dk>
2023-03-13 11:07:24 +0100

    Checking in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import ShoppingCart from './pages/ShoppingCart'
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <div>
-      <ShoppingCart   />  
+      <Checkout   />  
     </div>
   );
 }
---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import React from "react";
+
+
+
+
+export default function Checkout() {
+
+
+    return (
+        <>
+            <form>
+                <label>Land</label>
+                <input></input>
+                <label>Post nummer</label>
+                <input></input>
+                <label>By</label>
+                <input></input>
+                <label>Adresse</label>
+                <input></input>
+                <label>Telefon Nummer</label>
+                <input></input>
+                <label>Navn</label>
+                <input></input>
+                <label>Email</label>
+                <input></input>
+                <label>Firma navn</label>
+                <input></input>
+                <label>CVR nummer</label>
+                <input></input>
+            </form>
+
+        </>
+    )
+  }
