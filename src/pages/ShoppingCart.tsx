ec9a6b5acbf2018212d827a76d0e1b2c5929df78
andreasdan <andreas.petersen01@gmail.com>
2023-03-13 12:18:52 +0100

    fix merge mistake

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -7,19 +7,15 @@ import React from "react";
 import ShoppingCartSummary from "../components/ShoppingCartSummary";
 
 export default function ShoppingCart() {
-
-    const [items, setItems] = useState<Item[]>(cartItems);
-
-    return (
-        <>
-            <ShoppingCartTable items={items} setItems={setItems}/>
-            <ShoppingCartSummary items={items} />
-            <button><Link to="/checkout">Gå Til Kassen</Link></button>
+  const [items, setItems] = useState<Item[]>(cartItems);
 
   return (
     <>
       <ShoppingCartTable items={items} setItems={setItems} />
       <ShoppingCartSummary items={items} />
+      <button>
+        <Link to="/checkout">Gå Til Kassen</Link>
+      </button>
     </>
   );
 }
