26bd0a916ecf6b6f843de93b2c3163ec167d70d4
KingBendico <cph-bc105@cphbusiness.dk>
2023-03-28 18:59:35 +0200

    fixed a build error

--- src/pages/ShoppingCart/ShoppingCart.test.tsx
+++ src/pages/ShoppingCart/ShoppingCart.test.tsx
@@ -1,10 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import { Item } from "../../types/types";
 import ShoppingCart from "./ShoppingCart";
 
 describe(ShoppingCart.name, () => {
   it("should render", () => {
-    render(<ShoppingCart />);
+    render(<ShoppingCart items={[]} setItems={function (items: Item[]): void {
+      throw new Error("Function not implemented.");
+    } } />);
     expect(screen.getByLabelText("Artist name:")).toBeInTheDocument();
   });
 });
