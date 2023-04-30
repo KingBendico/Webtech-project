34d57993c761890eb29909b65d9e0e6ed08007b6
KingBendico <cph-bc105@cphbusiness.dk>
2023-04-30 22:00:12 +0200

    added 'success page', nothing fancy

--- src/pages/PaymentStatus/Success.tsx
+++ src/pages/PaymentStatus/Success.tsx
@@ -5,7 +5,7 @@ import ShoppingCartTable from "../../components/ShoppingCartTable/ShoppingCartTa
 export default function Success() {
   return (
     <>
-    
+    <p>Din betaling er gået igennem. Vi behandler din ordre så hurtigt som muligt</p>
     </>
   );
 }
